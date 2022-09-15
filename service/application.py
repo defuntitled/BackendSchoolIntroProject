49c6b2bf28dc894fb3790c0bb7c93516cbb5a78d
kirill <kirya.bikeev.03@mail.ru>
2022-09-15 12:00:08 +0300

    deploying and testing

--- service/application.py
+++ service/application.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask, request, make_response
 from service.db.db_session import global_init
 from service.app.item_finder import get_item_by_id
@@ -26,7 +27,7 @@ def get_node(id: str):
         database_loaded = True
     res = get_item_by_id(id)
     if not res:
-        return bad_answer(404, "Item not found")
+        flask.abort(404)
     else:
         return res, "200"
 
@@ -38,13 +39,16 @@ def import_nodes():
         global_init("db\\nodes.sqlite3")
         database_loaded = True
     if not request.json:
-        return bad_answer(400, "Validation Failed")
-    content = request.json
+        flask.abort(400)
+    try:
+        content = request.json
+    except:
+        flask.abort(400)
     res = import_items(content)
     if res:
         return "200"
     else:
-        return bad_answer(400, "Validation Failed")
+        flask.abort(400)
 
 
 @app.route("/delete/<string:id>", methods=["DELETE"])
@@ -55,7 +59,7 @@ def delete_node(id):
         database_loaded = True
     res = delete_item_by_id(id)
     if not res:
-        return bad_answer(404, "Item not found")
+        flask.abort(404)
     return "200"
 
 
